4a1bddf49f48a9122d46ced05c7aa8ae32928b97
SHarsheev <51026122+sharsheevnursultan@users.noreply.github.com>
2019-12-25 20:12:45 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 'use strict';
 
 function Tabs() {
@@ -32,6 +33,111 @@ let connectTabs = new Tabs();
 
 function addMenuActive() {
     let event = document.getElementById('menu-btn');
-    event.classList.toggle('activeMenu');
-
+    event.classList.toggle('active');
+    console.log('worked')
 }
+
+// spoiler script
+
+$(document).ready(function(){
+    $('.btn-spoiler').click(function(){ // при клике по заголовку спойлера
+        var show = $(this).attr('show'); // проверяем атрибут, в котором записано - отображен спойлер или скрыт
+        if(show == 1){ // если отображен, то прячем
+            $(this).attr('show', 0);
+            $(this).next('.spoiler-text').slideUp(300);
+        }else{ // если спрятан, то показываем
+            $(this).attr('show', 1);
+            $(this).next('.spoiler-text').slideDown(300);
+        }
+    });
+});
+
+$(document).ready(function(){
+    $('.btn-spoiler-left-menu').click(function(){ // при клике по заголовку спойлера
+        var show = $(this).attr('show'); // проверяем атрибут, в котором записано - отображен спойлер или скрыт
+        if(show == 1){ // если отображен, то прячем
+            $(this).attr('show', 0);
+            $(this).next('.spoiler-text').slideUp(300);
+        }else{ // если спрятан, то показываем
+            $(this).attr('show', 1);
+            $(this).next('.spoiler-text').slideDown(300);
+        }
+    });
+});
+
+// input type number
+
+function catalogItemCounter(field){
+
+    var fieldCount = function(el) {
+
+        var
+            // Мин. значение
+            min = el.data('min') || false,
+
+            // Макс. значение
+            max = el.data('max') || false,
+
+            // Кнопка уменьшения кол-ва
+            dec = el.prev('.dec'),
+
+            // Кнопка увеличения кол-ва
+            inc = el.next('.inc');
+
+        function init(el) {
+            if(!el.attr('disabled')){
+                dec.on('click', decrement);
+                inc.on('click', increment);
+            }
+
+            // Уменьшим значение
+            function decrement() {
+                var value = parseInt(el[0].value);
+                value--;
+
+                if(!min || value >= min) {
+                    el[0].value = value;
+                }
+            }
+
+            // Увеличим значение
+            function increment() {
+                var value = parseInt(el[0].value);
+
+                value++;
+
+                if(!max || value <= max) {
+                    el[0].value = value++;
+                }
+            }
+
+        }
+
+        el.each(function() {
+            init($(this));
+        });
+    };
+
+    $(field).each(function(){
+        fieldCount($(this));
+    });
+}
+
+catalogItemCounter('.fieldCount');
+
+// Галерея изображений на странице каталог
+
+thumbs.onclick = function(event) {
+    let thumbnail = event.target.closest('a');
+
+    if (!thumbnail) return;
+    showThumbnail(thumbnail.href, thumbnail.title);
+    event.preventDefault();
+};
+
+function showThumbnail(href, title) {
+    largeImg.src = href;
+    largeImg.alt = title;
+}
+
+
